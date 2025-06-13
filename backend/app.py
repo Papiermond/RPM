20c09323285ff0ff0df364afc397bbcf94621959
Fabian_Enrico_Lerchl <fabianlerchl7@gmail.com>
2025-06-13 15:54:20 +0200

    init

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Allows requests from Vue frontend
+
+@app.route('/api/data')
+def get_data():
+    return jsonify({"message": "Hello from Flask!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    #init
---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: '' }
+  },
+  mounted() {
+    fetch('http://localhost:5000/api/data')
+      .then(res => res.json())
+      .then(data => { this.message = data.message });
+  }
+}
+</script>
