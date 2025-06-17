b125ca3cbc478697e033583a239eb0a1c94ebd74
fabianlerchl <fabianlerchl27@gmail.com>
2025-06-17 05:01:30 +0200

    updated example

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,19 @@ CORS(app)  # Allows requests from Vue frontend
 
 @app.route('/api/data')
 def get_data():
-    return jsonify({"message": "Hello from Flask!"})
+    return jsonify({"message": "Hello from Data"})
+
+@app.route('/api/auth')
+def get_auth():
+    return jsonify({"message": "Hello from Authentication"})
+
+@app.route('/api/expo')
+def get_expoerter():
+    return jsonify({"message": "Hello from exporter"})
+
+@app.route('/api/download')
+def get_downloader():
+    return jsonify({"message": "Hello from download"})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div>{{ message }}</div>
+  <div>{{ message_auth }}</div>
+  <div>{{ message_expo }}</div>
+  <div>{{ message_download }}</div>
 </template>
 
 <script>
 export default {
   data() {
-    return { message: '' }
+    return { message: '',message_auth: '', message_expo: '',message_download: '' }
   },
   mounted() {
     fetch('http://localhost:5000/api/data')
       .then(res => res.json())
       .then(data => { this.message = data.message });
+    fetch('http://localhost:5000/api/auth')
+      .then(res => res.json())
+      .then(data => { this.message_auth = data.message });
+    fetch('http://localhost:5000/api/download')
+      .then(res => res.json())
+      .then(data => { this.message_download = data.message });
+    fetch('http://localhost:5000/api/expo')
+      .then(res => res.json())
+      .then(data => { this.message_expo = data.message });
   }
+
 }
 </script>
